5459575dafc28ee27b191f07d445df2442657f7c
gleesonm1 <matthew.gleeson.geo@gmail.com>
2023-05-17 12:10:34 -0700

    v0.2.0 includes update to output phase compositions

--- src/functions.jl
+++ src/functions.jl
@@ -41,17 +41,17 @@ function PT_minimisation(P_kbar, T_C, bulk)
 		out = point_wise_minimization(P_kbar, T_C, gv, z_b, DB, splx_data, sys_in);
 		Phase = out.ph
 		Oxides = out.oxides
-		if "liq" in Phase
-    			Liq_index = findfirst(x -> occursin("liq", x), out.ph)
-    			Liq_Comp = out.SS_vec[Liq_index].Comp_wt
-    			Liq_Frac = out.ph_frac_wt[Liq_index]
 		
-			finalize_MAGEMin(gv,DB);
-			Ret = Dict("Phase" => Phase, "Oxides" => Oxides, "Liq_Comp" => Liq_Comp, "Liq_Frac" => Liq_Frac)
-			return Ret
-		else
-    			finalize_MAGEMin(gv, DB);
-			Ret = Dict("Phase" => Phase, "Oxides" => Oxides)
-			return Ret
+		finalize_MAGEMin(gv, DB);
+		Ret = Dict();
+		Ret['sys'] = Dict("Phase" => Phase, "Oxides" => Oxides, "Comp" => out.bulk, "Entropy" => out.entropy);
+
+		for p in Phase
+			index = findfirst(x -> occursin(string(p), x), Phase);
+			Comp = out.SS_vec[index].Comp_wt;
+			Frac = out.ph_frac_wt[index];
+			
+			Ret[p] = Dict("Frac" =>, "Comp" => Comp);
 		end
+		return Ret
 	end
