611f2ac712e4a7a3e7d3694526f0d7d1fc74f0f1
gleesonm1 <matthew.gleeson.geo@gmail.com>
2023-05-22 15:20:22 -0700

    fudging Cr

--- src/Crystallisation.jl
+++ src/Crystallisation.jl
@@ -121,6 +121,9 @@ function path(bulk, T_C, P_kbar, Frac)
             bulk = [comp["SiO2"], comp["Al2O3"], comp["CaO"], comp["MgO"], comp["FeO"], comp["K2O"], comp["Na2O"], comp["TiO2"], comp["O"], comp["Cr2O3"], comp["H2O"]]
             new_bulk = 100*bulk/sum(bulk)
             new_bulk = round.(new_bulk, digits = 3)
+            if new_bulk[10] < 0.01
+                new_bulk[10] = 0.01
+            end
             print(new_bulk)
         end
     end
