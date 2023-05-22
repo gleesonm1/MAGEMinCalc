cc22aa7eec564af4389153408fefb017386e3e67
gleesonm1 <matthew.gleeson.geo@gmail.com>
2023-05-22 13:14:34 -0700

    v0.2.19

--- src/Crystallisation.jl
+++ src/Crystallisation.jl
@@ -4,6 +4,60 @@ using Roots
 using Polynomials
 using Pandas
 
+function findliq(bulk, P_kbar, T_start_C)
+    bulk_in = bulk;
+    gv, z_b, DB, splx_data = init_MAGEMin("ig");
+	sys_in = "wt";
+
+	gv.verbose = -1;
+
+	new_bulk = bulk/sum(bulk);
+	new_bulk_ox = ["SiO2"; "Al2O3"; "CaO"; "MgO"; "FeO"; "K2O"; "Na2O"; "TiO2"; "O"; "Cr2O3"; "H2O"]
+
+    T = T_start_C
+
+    gv = define_bulk_rock(gv, new_bulk, new_bulk_ox, sys_in, "ig");	
+    out = point_wise_minimization(P_kbar, T, gv, z_b, DB, splx_data, sys_in);
+
+    Liq = ["liq", "fl"]
+    PhaseList = out.ph
+
+    i = intersect(Liq, PhaseList)
+
+    Step = [3, 1, 0.1]
+    for k in eachindex(Step)
+        if length(i) == length(PhaseList)
+            while length(i) == length(PhaseList)
+                bulk = bulk_in
+                new_bulk = bulk/sum(bulk)
+                T = T - Step[k]
+                gv = define_bulk_rock(gv, new_bulk, new_bulk_ox, sys_in, "ig");	
+                out = point_wise_minimization(P_kbar, T, gv, z_b, DB, splx_data, sys_in);
+
+                PhaseList = out.ph
+                i = intersect(Liq, PhaseList)
+            end
+        end
+        if length(i) < length(PhaseList)
+            while length(i) < length(PhaseList)
+                bulk = bulk_in
+                new_bulk = bulk/sum(bulk)
+                T = T + Step[k]
+                gv = define_bulk_rock(gv, new_bulk, new_bulk_ox, sys_in, "ig");	
+                out = point_wise_minimization(P_kbar, T, gv, z_b, DB, splx_data, sys_in);
+
+                PhaseList = out.ph
+                i = intersect(Liq, PhaseList)
+            end
+
+        end
+    end
+
+    finalize_MAGEMin(gv, DB);
+    T_Liq = T
+    return T_Liq
+end
+
 function path(bulk, T_C, P_kbar, Frac)
     bulk_in = bulk
     gv, z_b, DB, splx_data = init_MAGEMin("ig");
