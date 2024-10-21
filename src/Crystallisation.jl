938704d34204679b8756ce9465dcfba6aa60325a
Matthew Gleeson <gleesonm@berkeley.edu>
2024-10-20 18:21:15 -0700

    v0.3.4

--- src/Crystallisation.jl
+++ src/Crystallisation.jl
@@ -3,6 +3,63 @@ using MAGEMin_C
 using Polynomials
 using Pandas
 
+function equilibrate(bulk, P_kbar, T_C)
+    if isa(bulk, Matrix{<:AbstractFloat})
+        new_bulk = [bulk[i, :] for i in 1:size(bulk, 1)]
+    else
+        new_bulk = bulk
+    end
+
+    println(new_bulk)
+    
+    println(typeof(new_bulk))
+	new_bulk_ox = ["SiO2"; "Al2O3"; "CaO"; "MgO"; "FeO"; "K2O"; "Na2O"; "TiO2"; "O"; "Cr2O3"; "H2O"]
+
+    data = Initialize_MAGEMin("ig", verbose = false)
+    out = multi_point_minimization(P_kbar, T_C, data, X = new_bulk, Xoxides = new_bulk_ox, sys_in = "wt")
+
+    Results = Dict()
+    Results["Conditions"] = DataFrame(columns = ["T_C", "P_kbar"], data = zeros(length(T_C), 2));
+    Results["sys"] = DataFrame(columns = new_bulk_ox, data = zeros(length(T_C), length(new_bulk_ox)));
+
+    for k in eachindex(T_C)
+        Phase = out[k].ph;
+        Oxides = out[k].oxides;
+        Type = out[k].ph_type;
+        
+        iloc(Results["Conditions"])[k] = Dict("T_C" => T_C[k], "P_kbar" => P_kbar[k]);
+        iloc(Results["sys"])[k] = Dict(zip(Oxides, out[k].bulk));
+        
+        if length(Phase) > 0	
+            i = 0
+            j = 0
+            for index in eachindex(Phase)
+                if !(Phase[index] in keys(Results))
+                    Results[string(Phase[index])] = DataFrame(columns = new_bulk_ox, data = zeros(length(T_C), length(new_bulk_ox)));
+                    Results[string(Phase[index],"_prop")] = DataFrame(columns = ["mass"], data = zeros(length(T_C), 1));
+                end
+
+                Frac = out[k].ph_frac_wt[index];
+                iloc(Results[string(Phase[index],"_prop")])[k] = Dict("mass" => Frac);
+                if Type[index] == 0
+                    i = i + 1
+                    Comp = out[k].PP_vec[i].Comp_wt;
+                    iloc(Results[Phase[index]])[k] = Dict(zip(Oxides,Comp));
+                else
+                    j = j +1
+                    Comp = out[k].SS_vec[j].Comp_wt;
+                    iloc(Results[Phase[index]])[k] = Dict(zip(Oxides,Comp));
+                end
+            end
+        end
+    end
+
+	#finalize_MAGEMin(gv, DB);
+    Finalize_MAGEMin(data)
+	return Results
+
+end
+
 function findliq(bulk, P_kbar, T_start_C)
     bulk_in = bulk;
     # gv, z_b, DB, splx_data = init_MAGEMin("ig");
