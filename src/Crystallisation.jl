fa49b6d78c272711acceebfd4f9dc6a95795cb67
Matthew Gleeson <gleesonm@berkeley.edu>
2024-12-16 19:30:23 -0800

    v0.3.6

--- src/Crystallisation.jl
+++ src/Crystallisation.jl
@@ -31,30 +31,34 @@ function equilibrate(bulk, P_kbar, T_C)
         iloc(Results["sys"])[k] = Dict(zip(Oxides, out[k].bulk));
         
         if length(Phase) > 0	
+            phase_counts = Dict{String, Int}()  # Counter for phases
             i = 0
             j = 0
             for index in eachindex(Phase)
-                if !(Phase[index] in keys(Results))
-                    Results[string(Phase[index])] = DataFrame(columns = new_bulk_ox, data = zeros(length(T_C), length(new_bulk_ox)));
-                    Results[string(Phase[index],"_prop")] = DataFrame(columns = ["mass"], data = zeros(length(T_C), 1));
+                phase_name = string(Phase[index])
+                phase_counts[phase_name] = get(phase_counts, phase_name, 0) + 1
+                unique_phase_name = string(phase_name, phase_counts[phase_name])
+
+                if !(unique_phase_name in keys(Results))
+                    Results[unique_phase_name] = DataFrame(columns = new_bulk_ox, data = zeros(length(T_C), length(new_bulk_ox)));
+                    Results[string(unique_phase_name,"_prop")] = DataFrame(columns = ["mass"], data = zeros(length(T_C), 1));
                 end
 
                 Frac = out[k].ph_frac_wt[index];
-                iloc(Results[string(Phase[index],"_prop")])[k] = Dict("mass" => Frac);
+                iloc(Results[string(unique_phase_name,"_prop")])[k] = Dict("mass" => Frac);
                 if Type[index] == 0
                     i = i + 1
                     Comp = out[k].PP_vec[i].Comp_wt;
-                    iloc(Results[Phase[index]])[k] = Dict(zip(Oxides,Comp));
+                    iloc(Results[unique_phase_name])[k] = Dict(zip(Oxides,Comp));
                 else
                     j = j +1
                     Comp = out[k].SS_vec[j].Comp_wt;
-                    iloc(Results[Phase[index]])[k] = Dict(zip(Oxides,Comp));
+                    iloc(Results[unique_phase_name])[k] = Dict(zip(Oxides,Comp));
                 end
             end
         end
     end
 
-	#finalize_MAGEMin(gv, DB);
     Finalize_MAGEMin(data)
 	return Results
 
@@ -62,36 +66,25 @@ end
 
 function findliq(bulk, P_kbar, T_start_C)
     bulk_in = bulk;
-    # gv, z_b, DB, splx_data = init_MAGEMin("ig");
-	# sys_in = "wt";
-
-	# gv.verbose = -1;
 
 	new_bulk = bulk/sum(bulk);
-	new_bulk_ox = ["SiO2"; "Al2O3"; "CaO"; "MgO"; "FeO"; "K2O"; "Na2O"; "TiO2"; "O"; "Cr2O3"; "H2O"]
+	new_bulk_ox = ["SiO2"; "Al2O3"; "CaO"; "MgO"; "FeO"; "K2O"; "Na2O"; "TiO2"; "O"; "Cr2O3"; "H2O"];
 
-    T = T_start_C
+    T = T_start_C;
 
-    # gv = define_bulk_rock(gv, new_bulk, new_bulk_ox, sys_in, "ig");	
-    # out = point_wise_minimization(P_kbar, T, gv, z_b, DB, splx_data, sys_in);
+    data = Initialize_MAGEMin("ig", verbose = false);
+    out = single_point_minimization(P_kbar, T, data, X = new_bulk, Xoxides = new_bulk_ox, sys_in = "wt");
 
-    data = Initialize_MAGEMin("ig", verbose = false)
-    out = single_point_minimization(P_kbar, T, data, X = new_bulk, Xoxides = new_bulk_ox, sys_in = "wt")
+    Liq = ["liq", "fl"];
+    PhaseList = out.ph;
 
-    Liq = ["liq", "fl"]
-    PhaseList = out.ph
+    i = intersect(Liq, PhaseList);
 
-    i = intersect(Liq, PhaseList)
-
-    Step = [3, 1, 0.1]
+    Step = [3, 1, 0.1];
     for k in eachindex(Step)
         if length(i) === length(PhaseList)
             while length(i) === length(PhaseList)
-                # bulk = bulk_in
-                # new_bulk = bulk/sum(bulk)
                 T = T - Step[k]
-                # gv = define_bulk_rock(gv, new_bulk, new_bulk_ox, sys_in, "ig");	
-                # out = point_wise_minimization(P_kbar, T, gv, z_b, DB, splx_data, sys_in);
                 out = single_point_minimization(P_kbar, T, data, X = new_bulk, Xoxides = new_bulk_ox, sys_in = "wt")
 
                 PhaseList = out.ph
@@ -106,23 +99,8 @@ function findliq(bulk, P_kbar, T_start_C)
         i = intersect(Liq, PhaseList)
 
         T = T - Step[k]-2
-
-        # if length(i) < length(PhaseList)
-        #     while length(i) < length(PhaseList)
-        #         bulk = bulk_in
-        #         new_bulk = bulk/sum(bulk)
-        #         T = T + Step[k]
-        #         # gv = define_bulk_rock(gv, new_bulk, new_bulk_ox, sys_in, "ig");	
-        #         # out = point_wise_minimization(P_kbar, T, gv, z_b, DB, splx_data, sys_in);
-        #         out = single_point_minimization(P_kbar, T, data, X = new_bulk, Xoxides = new_bulk_ox, sys_in = "wt")
-
-        #         PhaseList = out.ph
-        #         i = intersect(Liq, PhaseList)
-        #     end
-        # end
     end
 
-    # finalize_MAGEMin(gv, DB);
     Finalize_MAGEMin(data)
     T_Liq = T
     return T_Liq
@@ -131,10 +109,6 @@ end
 function path(bulk, T_C, P_kbar, Frac, phases)
     Choice = Frac;
     bulk_in = bulk;
-    # gv, z_b, DB, splx_data = init_MAGEMin("ig");
-	# sys_in = "wt";
-
-	# gv.verbose = -1;
 
 	new_bulk = 100*bulk/sum(bulk);
 	new_bulk_ox = ["SiO2"; "Al2O3"; "CaO"; "MgO"; "FeO"; "K2O"; "Na2O"; "TiO2"; "O"; "Cr2O3"; "H2O"]
@@ -146,16 +120,8 @@ function path(bulk, T_C, P_kbar, Frac, phases)
     data = Initialize_MAGEMin("ig", verbose = false)
 
     for k in eachindex(T_C)
-        # gv, z_b, DB, splx_data = init_MAGEMin("ig");
-        # sys_in = "wt";
-
-        
         out = single_point_minimization(P_kbar[k], T_C[k], data, X = new_bulk, Xoxides = new_bulk_ox, sys_in = "wt")
     
-        # gv.verbose = -1;
-        # gv = define_bulk_rock(gv, new_bulk, new_bulk_ox, sys_in, "ig");	
-        # out = point_wise_minimization(P_kbar[k], T_C[k], gv, z_b, DB, splx_data, sys_in);
-
         Phase = out.ph;
         Oxides = out.oxides;
         Type = out.ph_type;
@@ -163,25 +129,31 @@ function path(bulk, T_C, P_kbar, Frac, phases)
         iloc(Results["Conditions"])[k] = Dict("T_C" => T_C[k], "P_kbar" => P_kbar[k]);
         iloc(Results["sys"])[k] = Dict(zip(Oxides, out.bulk));
         
-        if length(Phase) > 0	
+        if length(Phase) > 0
+            phase_counts = Dict{String, Int}()  # Counter for phases
+
             i = 0
             j = 0
             for index in eachindex(Phase)
-                if !(Phase[index] in keys(Results))
-                    Results[string(Phase[index])] = DataFrame(columns = new_bulk_ox, data = zeros(length(T_C), length(new_bulk_ox)));
-                    Results[string(Phase[index],"_prop")] = DataFrame(columns = ["mass"], data = zeros(length(T_C), 1));
+                phase_name = string(Phase[index])
+                phase_counts[phase_name] = get(phase_counts, phase_name, 0) + 1
+                unique_phase_name = string(phase_name, phase_counts[phase_name])
+
+                if !(unique_phase_name in keys(Results))
+                    Results[unique_phase_name] = DataFrame(columns = new_bulk_ox, data = zeros(length(T_C), length(new_bulk_ox)));
+                    Results[string(unique_phase_name,"_prop")] = DataFrame(columns = ["mass"], data = zeros(length(T_C), 1));
                 end
 
                 Frac = out.ph_frac_wt[index];
-                iloc(Results[string(Phase[index],"_prop")])[k] = Dict("mass" => Frac);
+                iloc(Results[string(unique_phase_name,"_prop")])[k] = Dict("mass" => Frac);
                 if Type[index] == 0
                     i = i + 1
                     Comp = out.PP_vec[i].Comp_wt;
-                    iloc(Results[Phase[index]])[k] = Dict(zip(Oxides,Comp));
+                    iloc(Results[unique_phase_name])[k] = Dict(zip(Oxides,Comp));
                 else
                     j = j +1
                     Comp = out.SS_vec[j].Comp_wt;
-                    iloc(Results[Phase[index]])[k] = Dict(zip(Oxides,Comp));
+                    iloc(Results[unique_phase_name])[k] = Dict(zip(Oxides,Comp));
                 end
             end
         end
@@ -192,14 +164,14 @@ function path(bulk, T_C, P_kbar, Frac, phases)
         end
 
         if Choice == 1
-            comp = iloc(Results["liq"])[k]
+            comp = iloc(Results["liq1"])[k]
             bulk = [comp["SiO2"], comp["Al2O3"], comp["CaO"], comp["MgO"], comp["FeO"], comp["K2O"], comp["Na2O"], comp["TiO2"], comp["O"], comp["Cr2O3"], comp["H2O"]]
             new_bulk = 100*bulk/sum(bulk)
             new_bulk = round.(new_bulk, digits = 3)
-            if new_bulk[10] < 0.01
-                new_bulk[10] = 0.01
-            end
-            print(new_bulk)
+            # if new_bulk[10] < 0.01
+            #     new_bulk[10] = 0.01
+            # end
+            # print(new_bulk)
         end
 
         if phases !== 0
@@ -216,7 +188,6 @@ function path(bulk, T_C, P_kbar, Frac, phases)
         end
     end
 
-	#finalize_MAGEMin(gv, DB);
     Finalize_MAGEMin(data)
 	return Results
 end
--- src/Melting.jl
+++ src/Melting.jl
@@ -46,24 +46,99 @@ using BasicInterpolators
 #     return out, Test
 # end
 
+function optimize_entropy(T, s, P, data, bulk, bulk_ox, n)
+    T_save = range(T, step=-0.75, length=n)
+    P_test = fill(P, n)
+    out_save = multi_point_minimization(P_test, T_save, data, X=bulk, Xoxides=bulk_ox, sys_in="wt", progressbar=false)
+
+    # Filter and fit polynomial to estimate T
+    s_save = [out.entropy for out in out_save]
+    mask = .!ismissing.(s_save)
+    coeffs = fit(s_save[mask], T_save[mask], 3)
+    return coeffs(s)
+end
+
+function AdiabaticDecompressionMelting_new(bulk::Vector{Float64}, T_start_C::Float64, P_start_kbar::Float64, P_end_kbar::Float64, dp_kbar::Float64, Frac::Float64)
+    # Precompute pressure range
+    P = range(P_start_kbar, step=-dp_kbar, stop=P_end_kbar)
+
+    # Normalize bulk composition
+    new_bulk = bulk / sum(bulk)
+    new_bulk_ox = ["SiO2", "Al2O3", "CaO", "MgO", "FeO", "K2O", "Na2O", "TiO2", "O", "Cr2O3", "H2O"]
+
+    # Initialize system
+    data = Initialize_MAGEMin("ig", verbose=false)
+    Results = Dict("Conditions" => DataFrame((T_C=zeros(length(P)), P_kbar=zeros(length(P)), s=zeros(length(P)))),
+        "sys" => DataFrame(zeros(length(P), length(new_bulk_ox)), columns=new_bulk_ox))
+
+    T = T_start_C
+    out = single_point_minimization(P[1], T, data, X=new_bulk, Xoxides=new_bulk_ox, sys_in="wt")
+    s = out.entropy
+
+    for (k, pressure) in enumerate(P)
+        # Update bulk
+        new_bulk = bulk / sum(bulk)
+
+        if k > 1
+            # Converge entropy via minimization
+            n = 4
+            T_next = optimize_entropy(T, s, pressure, data, new_bulk, new_bulk_ox, n)
+
+            # Update temperature for next step
+            out = single_point_minimization(pressure, T_next, data, X=new_bulk, Xoxides=new_bulk_ox, sys_in="wt")
+
+            if abs(out.entropy - s)/s > 0.0001
+                while abs(out.entropy - s)/s > 0.0001
+                    n = n*2
+                    T_next, s_check = optimize_entropy(T, s, pressure, data, new_bulk, new_bulk_ox, n)
+
+                    # Update temperature for next step
+                    out = single_point_minimization(pressure, T_next, data, X=new_bulk, Xoxides=new_bulk_ox, sys_in="wt")
+                    if n > 30
+                        break
+                    end
+                end
+            end
+            T = T_next
+        end
+
+        # Record conditions and system results
+        Results["Conditions"][k, :] .= [T, pressure, out.entropy]
+        Results["sys"][k, :] .= out.bulk
+
+        phase_counts = Dict{String, Int}()
+
+        # Handle phases
+        for (index, phase) in enumerate(out.ph)  # Counter for PhaseList
+            phase_name = string(phase)
+            phase_counts[phase_name] = get(phase_counts, phase_name, 0) + 1
+            unique_phase_name = string(phase_name, phase_counts[phase_name])
+
+            if !(phase in keys(Results))
+                Results[unique_phase_name] = DataFrame(zeros(length(P), length(new_bulk_ox)), columns=new_bulk_ox)
+                Results["$(unique_phase_name)_prop"] = DataFrame(mass=zeros(length(P)))
+            end
+            Results["$(unique_phase_name)_prop"][k, :] .= out.ph_frac_wt[index]
+            Results[unique_phase_name][k, :] .= out.ph_type[index] == 0 ? out.PP_vec[index].Comp_wt : out.SS_vec[index].Comp_wt
+        end
+    end
+
+    Finalize_MAGEMin(data)
+    return Results
+end
+
+
 function AdiabaticDecompressionMelting(bulk, T_start_C, P_start_kbar, P_end_kbar, dp_kbar, Frac)
     P = collect(range(P_start_kbar, P_end_kbar, 1+round(Int,(P_start_kbar - P_end_kbar)/dp_kbar)));
     bulk_in = bulk
-    # gv, z_b, DB, splx_data = init_MAGEMin("ig");
-	# sys_in = "wt";
-
-	# gv.verbose = -1;
 
 	new_bulk = bulk/sum(bulk);
-	new_bulk_ox = ["SiO2"; "Al2O3"; "CaO"; "MgO"; "FeO"; "K2O"; "Na2O"; "TiO2"; "O"; "Cr2O3"; "H2O"]
-
-    # gv = define_bulk_rock(gv, new_bulk, new_bulk_ox, sys_in, "ig");	
+	new_bulk_ox = ["SiO2"; "Al2O3"; "CaO"; "MgO"; "FeO"; "K2O"; "Na2O"; "TiO2"; "O"; "Cr2O3"; "H2O"]	
 
     data = Initialize_MAGEMin("ig", verbose = false)
 
     T = T_start_C
     Results = Dict()
-    # out = point_wise_minimization(P[1], T, gv, z_b, DB, splx_data, sys_in);#PT_minimisation(P[1], T, bulk); #point_wise_minimization(P[1], T, gv, z_b, DB, splx_data, sys_in);
     out = single_point_minimization(P[1], T, data, X = new_bulk, Xoxides = new_bulk_ox, sys_in = "wt")
     s = out.entropy
     println(s)
@@ -191,8 +266,7 @@ function AdiabaticDecompressionMelting(bulk, T_start_C, P_start_kbar, P_end_kbar
             end
             T = T_next
             
-        end  
-        # println(P[k])      
+        end       
     
         Phase = out.ph;
         Oxides = out.oxides;
@@ -202,32 +276,37 @@ function AdiabaticDecompressionMelting(bulk, T_start_C, P_start_kbar, P_end_kbar
         iloc(Results["sys"])[k] = Dict(zip(Oxides, out.bulk));
         
         if length(Phase) > 0	
+            phase_counts = Dict{String, Int}()  # Counter for phases
+
             i = 0
             j = 0
             for index in eachindex(Phase)
-                if !(Phase[index] in keys(Results))
+                phase_name = string(Phase[index])
+                phase_counts[phase_name] = get(phase_counts, phase_name, 0) + 1
+                unique_phase_name = string(phase_name, phase_counts[phase_name])
+
+                if !(unique_phase_name in keys(Results))
                     # Results[string(Phase[index])] = create_dataframe(new_bulk_ox, length(P))  
                     # Results[string(Phase[index],"_prop")] = create_dataframe(["Mass"], length(P)) 
-                    Results[string(Phase[index])] = DataFrame(columns = new_bulk_ox, data = zeros(length(P), length(new_bulk_ox)));
-                    Results[string(Phase[index],"_prop")] = DataFrame(columns = ["mass"], data = zeros(length(P), 1));
+                    Results[unique_phase_name] = DataFrame(columns = new_bulk_ox, data = zeros(length(P), length(new_bulk_ox)));
+                    Results[string(unique_phase_name,"_prop")] = DataFrame(columns = ["mass"], data = zeros(length(P), 1));
                 end
 
                 Fraction = out.ph_frac_wt[index];
-                iloc(Results[string(Phase[index],"_prop")])[k] = Dict("mass" => Fraction);
+                iloc(Results[string(unique_phase_name,"_prop")])[k] = Dict("mass" => Fraction);
                 if Type[index] == 0
                     i = i + 1
                     Comp = out.PP_vec[i].Comp_wt;
-                    iloc(Results[Phase[index]])[k] = Dict(zip(Oxides,Comp));
+                    iloc(Results[unique_phase_name])[k] = Dict(zip(Oxides,Comp));
                 else
                     j = j +1
                     Comp = out.SS_vec[j].Comp_wt;
-                    iloc(Results[Phase[index]])[k] = Dict(zip(Oxides,Comp));
+                    iloc(Results[unique_phase_name])[k] = Dict(zip(Oxides,Comp));
                 end
             end
         end
     end
 
-	# finalize_MAGEMin(gv, DB);
     Finalize_MAGEMin(data)
 	return Results
 end
